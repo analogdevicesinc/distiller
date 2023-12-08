2f62e7f97955abe5b96db0ab7f6e129b939a0e10
Robert Muchsel <16564465+rotx-maxim@users.noreply.github.com>
2023-12-08 09:16:30 -0600

    Update config.py

--- distiller/config.py
+++ distiller/config.py
@@ -1,5 +1,6 @@
 #
 # Copyright (c) 2018 Intel Corporation
+# Portions Copyright (C) 2023 Analog Devices, Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,14 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-###################################################################################################
-#
-# Portions Copyright (C) 2023 Analog Devices, Inc. All Rights Reserved.
-#
-# Analog Devices, Inc. Default Copyright Notice:
-# https://www.analog.com/en/about-adi/legal-and-risk-oversight/intellectual-property/copyright-notice.html
-#
-###################################################################################################
 
 """CompressionScheduler configuration parsing.
 
